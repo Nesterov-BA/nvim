7bea32d9bd9b70699515c9180be0e31e61a49cba
Boris <nesterov.boris123@gmail.com>
2025-07-30 22:33:29 +0300

    now coc handles all autocompletion

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,8 @@
+-- ~/.config/nvim/lua/plugins/autocompletion.lua
+return {
+  -- Disable blink.cmp
+  {
+    "Saghen/blink.cmp", -- or whatever the plugin's repo name is
+    enabled = false, -- This will prevent LazyVim from loading it
+  },
+}
